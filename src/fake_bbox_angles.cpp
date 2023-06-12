9e0d3fe5b36df29b554394938a7ceb18818bf50c
Grace Pearcey <pearcey.grace@gmail.com>
2023-06-11 20:10:47 -0400

    updating bbox angles to publish a red and green marker

--- src/fake_bbox_angles.cpp
+++ src/fake_bbox_angles.cpp
@@ -4,16 +4,29 @@
 #include <ros/console.h>
 
 
+
 void fake_bbox_angles() {
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<navigation_pkg::PropInProgress>("prop_angle_range", 1);
     ros::Rate rate(10);
     navigation_pkg::PropInProgress msg;
-    msg.prop_type = "marker";
-    msg.theta_1 = 60*(M_PI/180); 
-    msg.theta_2 = 120*(M_PI/180); 
+    bool red_marker = true;//variable used to switch between publishing red and green markers
+
+ 
 
     while (ros::ok()) {
+        if (red_marker){
+            msg.prop_type = "red_marker";
+            msg.theta_1 = 60*(M_PI/180); 
+            msg.theta_2 = 120*(M_PI/180);
+            red_marker = false;
+        }
+        else{
+            msg.prop_type = "green_marker";
+            msg.theta_1 = 60*(M_PI/180); 
+            msg.theta_2 = 120*(M_PI/180); 
+            red_marker = true;
+        }
         ROS_DEBUG_STREAM("fake_bbox_angles: theta_1 - " << msg.theta_1 << "theta_2 - " << msg.theta_2);
         pub.publish(msg);
         rate.sleep();
@@ -21,6 +34,7 @@ void fake_bbox_angles() {
 }
 
 int main(int argc, char** argv) {
+
     ros::init(argc, argv, "fake_bbox_angles_node");
     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
         ros::console::notifyLoggerLevelsChanged();
